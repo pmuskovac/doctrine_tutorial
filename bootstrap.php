9abb16bdf271a8b3d3df912eeec3f813ee647a72
Philip Muškovac <yofel@gmx.net>
2013-10-14 14:24:17 +0200

    initial import

---
+++ bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: yofel
+ * Date: 10/14/13
+ * Time: 12:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once("vendor/auotoload.php");
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
+// or if you prefer yaml or xml
+//$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
+//$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
+
+// database configuration parameters
+$conn = array(
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__ . '/db.sqlite',
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
